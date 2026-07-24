cdfb1cb542720a2673fcb65c16d38a54b990c9de
Anish Debnath <118295754+AnishDebnath@users.noreply.github.com>
2026-07-24 18:43:43 +0530

    Fix image showing error

---
+++ client/src/data/about/index.ts
@@ -0,0 +1,13 @@
+export type { CoreValue } from './coreValue';
+export { coreValues } from './coreValue';
+
+export type { Partner } from './partner';
+export { partners, homePartners } from './partner';
+
+export { storytellerImages } from './storyteller';
+
+export type { TeamMember } from './team';
+export { teamMembers } from './team';
+
+export type { USP } from './whyus';
+export { usps } from './whyus';
